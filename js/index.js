323570abf552b614b2023878a1c40eda7747e050
Saimon Thada <magarchsstm@gmail.com>
2018-02-02 15:45:53 +0530

    update

--- js/index.js
+++ js/index.js
@@ -53,7 +53,7 @@ function workInfo(){
                 duration = info[i].duration;
             }  
             var value = '<div class="w3-container">'+
-                            '<h5 class="w3-text-black w3-opacity"><b>'+info[i].role+'</b></h5>'+
+                            '<h5><b>'+info[i].role+'</b></h5>'+
                             '<h6 class="w3-text-teal"><i class="fa fa-building fa-fw w3-margin-right"></i>'+info[i].company+'</h6>'+
                             '<h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>'+duration+'</h6>'+
                             '<p>'+info[i].description+'</p>'+
